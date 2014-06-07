3ae9402ba698a30d92ea61b7d4c0b61558f415e2
Pandu <prendradjaja@gmail.com>
2014-06-07 16:02:22 -0700

    Add tic-tac-toe GUI

--- public/js/ui.js
+++ public/js/ui.js
@@ -17,10 +17,10 @@ function board_string(game) {
 }
 
 $(window).on('waiting_for_opponent', function(event) {
+    $('#connecting').css('display', 'none');
     console_print('Waiting for opponent...');
     console_print('Send your friend this url:');
     console_print(window.location.href);
-
 });
 
 function enable_console() {
@@ -41,30 +41,55 @@ $(window).on('invalid_move', function(event) {
 
 $(window).on('your_turn', function(event, move) {
     if (move === 0) {
-        enable_console();
+        game_start();
         console_print('Start! (your turn)');
     } else {
         console_print(board_string(game));
+        mark_cell(move.row, move.col, move.player);
         console_print('Your turn.');
     }
 });
 
 $(window).on('opponent_turn', function(event, move) {
     if (move === 0) {
-        enable_console();
+        game_start();
         console_print('Start! (opponent\'s turn)');
     } else {
         console_print(board_string(game));
+        mark_cell(move.row, move.col, move.player);
         console_print('Opponent\'s turn.');
     }
 });
 
 $(window).on('win', function(event, move) {
     console_print(board_string(game));
+    mark_cell(move.row, move.col, move.player);
     console_print('You win!');
 });
 
 $(window).on('lose', function(event, move) {
     console_print(board_string(game));
+    mark_cell(move.row, move.col, move.player);
     console_print('You lose.');
 });
+
+function game_start () {
+    $('#connecting').css('display', 'none');
+    enable_console();
+    $('.game-wrap').css('display', 'block');
+    $('.board-cell').click(function (a) {
+        var clicked = parse_cell_id(this.id);
+        $(window).trigger('make_move', new Move(player, clicked.row, clicked.col));
+    });
+}
+
+function parse_cell_id(id) {
+    var parts = id.split('-');
+    return {row: parseInt(parts[1]),
+            col: parseInt(parts[2])};
+}
+
+function mark_cell(row, col, player) {
+    var class_name = ['x', 'o'][player];
+    $('#cell-'+row+'-'+col).addClass(class_name);
+}
